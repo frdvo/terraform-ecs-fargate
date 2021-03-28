c60885ad3ba4435bbdca13b8e3cca94738d98689
Fernando Rolnik <fernando.rolnik@cmd.com.au>
2021-03-28 22:03:28 +1030

    Add my own container

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 
 variable "aws_region" {
   description = "The AWS region things are created in"
-  default     = "us-west-2"
+  default     = "ap-southeast-2"
 }
 
 variable "ecs_task_execution_role_name" {
@@ -12,17 +12,17 @@ variable "ecs_task_execution_role_name" {
 
 variable "az_count" {
   description = "Number of AZs to cover in a given region"
-  default     = "2"
+  default     = "3"
 }
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "861838963348.dkr.ecr.ap-southeast-2.amazonaws.com/weather-react:19dc7f9"
 }
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 3000
+  default     = 80
 }
 
 variable "app_count" {
@@ -36,11 +36,11 @@ variable "health_check_path" {
 
 variable "fargate_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024"
+  default     = "256"
 }
 
 variable "fargate_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "2048"
+  default     = "512"
 }
 
